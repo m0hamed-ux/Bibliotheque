f1abb5f2e28f7714bf6fa676d2ac6eeae6a85eb8
nu-ur <nourmes2005@gmail.com>
2025-01-08 21:51:33 +0100

    .......

--- App/Models and Gestion/bilio1.py
+++ App/Models and Gestion/bilio1.py
@@ -1,8 +1,6 @@
-from livre import *
-from Adherent import *
-from Auteur import *
 from Emprunt import *
-from datetime import date
+from Adherent import *
+from datetime import *
 class Biblio:
     def __init__(self):
         self.__livres=[]
@@ -12,17 +10,24 @@ class Biblio:
         adherent = self.rechercherAdherent(codeA)
         livre = self.rechercherLivre(codeL)
         if livre and adherent and livre.livreDisponible():
-            date_emprunt = date.today()
-            date_retour_prevue = date_emprunt + timedelta(days=3)
-            emprunt = Emprunt(livre, adherent, date_emprunt, date_retour_prevue)
-            self.emprunts.append(emprunt)
-            livre.set_nbr_exemplaire_disponible(livre.set_nbr_exemplaire_disponible-1)
-            return emprunt
-        return None
+            dateEmprunt = date.today()
+            dateRetourPrevue = dateEmprunt + timedelta(days=3)
+            emprunt = Emprunt(livre, adherent, dateEmprunt, dateRetourPrevue)
+            self.__emprunts.append(emprunt)
+            livre.set_nbr_exemplaire_disponible(livre.set_nbr_exemplaire_disponible()-1)
     def retourEmprunt(self,codeEmprunt):
         for elt in self.__emprunts:
             if elt.getCode()==codeEmprunt:
                 elt.setDateRetourEffective(date.today)
-                elt.set_nbr_exemplaire_disponible(elt.set_nbr_exemplaire_disponible+1)         
-
-        
+                elt.set_nbr_exemplaire_disponible(elt.set_nbr_exemplaire_disponible()+1)                         
+    def topEmprunts(self):
+        pass
+    def emprunteurs(self):
+        emprunteurs=[]
+        for elt in self.__emprunts:
+            if elt.etatEmprunt() in ["en cours","non rendu"]:
+                if elt.getEmprunteurLivre() not in emprunteurs:
+                    emprunteurs.append(elt.getEmprunteurLivre())
+        return emprunteurs
+    def datePossibilitéEmprunt(codeL):
+        pass           
