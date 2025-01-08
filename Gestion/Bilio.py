bbbc1af4bad22b7f03e2bfaa8892849c93dbbf78
nu-ur <nourmes2005@gmail.com>
2025-01-08 17:33:04 +0100

    .......

--- Gestion/Bilio.py
+++ Gestion/Bilio.py
@@ -1,3 +1,23 @@
 class Biblio:
     def __init__(self):
+        self.__livres=[]
+        self.__adherents=[]
+        self.__emprunts=[]
+    def ajouterLivre(self):
+        pass
+    def ajouterAdherent(self):
+        pass
+    def rechercherAdherent(self):
+        pass
+    def rechercherLivre(self):
+        pass
+    def ajouterEmprunt(self,codeA,codeL):
+        pass
+    def retourEmprunt(self,codeEmprunt):
+        pass   
+    def topEmprunts(self):
+        pass
+    def emprunteurs(self):
+        pass
+    def datePossibilitéEmprunt(self,codeL):
         pass
