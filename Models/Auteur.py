ef1cc58ac66d20a39db9b5d48fc2826db940327b
Mohamed <elkhamlichim099@gmail.com>
2025-01-08 15:44:26 +0100

    Auteur

--- Models/Auteur.py
+++ Models/Auteur.py
@@ -1,2 +1,19 @@
 from Personne import *
-class Auteur(Personne):
+from re import *
+class Auteur(Perssonne):
+    def _init_(self, nom, prenom, code):
+        if match(code, "^A[0-9]{4}$"):
+            super()._init_(nom, prenom)
+            self.__code = code
+        else:
+            raise Exception("Code invalide")
+    def getCode(self):
+        return self.__code
+    def setCode(self, code):
+        if match(code, "^A[0-9]{4}$"):
+            self.__code = code
+        else:
+            raise Exception("Code invalide")
+    def __str__(self):
+        return f"L'auteur {self.getCode()} : " + super().__str__()
+    
