9bfc76d8a06aeb409af0641035d23a6b334f9fd6
nu-ur <nourmes2005@gmail.com>
2025-01-08 17:01:44 +0100

    ........

---
+++ Models/Adherent.py
@@ -0,0 +1,20 @@
+from Personne import *
+from datetime import *
+from re import *
+class Adherent(Personne) :
+    code=0
+    def __init__(self,nom,prenom,DayAdhésion,monthAdhésion,yearAdhésion):
+        Adherent.code +=1
+        Personne._init_(self,nom,prenom)
+        if not date(yearAdhésion, monthAdhésion, DayAdhésion)== date.today():
+            raise Exception ("date inscription invalide")
+        else:
+            self.__DateAdhésion = date(yearAdhésion, monthAdhésion, DayAdhésion)
+            self.__code = Adherent.code
+        
+    def getCode(self):
+        return self.__code
+    def getDateDateAdhésion(self):
+        return self.__DateAdhésion
+    def __str__(self):
+        return super()._str()+f" / Le code est : {self.getCode()} et la date d'Adhésion : {self.DateAdhésion.day} . {self.DateAdhésion.month} . {self._DateAdhésion.year}"
