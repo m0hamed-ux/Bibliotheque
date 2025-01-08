f40254b5f4e61e0439dba609e6eadf6bf37c6682
Mohamed <elkhamlichim099@gmail.com>
2025-01-08 15:46:35 +0100

    Personne

--- Models/Personne.py
+++ Models/Personne.py
@@ -1,4 +1,30 @@
 class Perssonne:
     def _init_(self,nom,prenom):
+        if not isinstance(nom,str):
+            raise Exception("le nom est invalid!")
+        elif not isinstance(prenom,str):
+            raise Exception("le prenom est invalid!")
+        else:
+            self.nom=nom
+            self.prenom=prenom
+        
+    def get_nom(self,nom):
         self.nom=nom
-        self.prenom=prenom
+        
+    def set_nom(self,nom):
+        if not isinstance(nom,str):
+            raise Exception("le nom est invalid!")
+        else:
+            self.nom=nom
+        
+    def get_prenomm(self,nom):
+        self.nom=nom
+        
+    def set_prenom(self,prenom):
+        if not isinstance(prenom,str):
+            raise Exception("le prenom est invalid!")
+        else:
+            self.prenom=prenom
+    
+    def _str_(self):
+        return f"Nom : {self.nom} , Prenom : {self.prenom}"
